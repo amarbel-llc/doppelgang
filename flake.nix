1d4ee979ba0dd5179954acb3db977678621171a4
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-24 20:43:32 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     igloo.url = "github:amarbel-llc/igloo";
     igloo.inputs.nixpkgs-master.follows = "nixpkgs-master";
     igloo.inputs.treefmt-nix.follows = "treefmt-nix";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     utils.inputs.systems.follows = "igloo/systems";
     treefmt-nix.url = "github:numtide/treefmt-nix";
